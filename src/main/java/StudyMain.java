a9e76450712e571999aa8e9dc717ef1947a6058c
za-hejun <hejun@zhongan.io>
2018-04-09 20:05:15 +0800

    修改类名

--- src/main/java/StudyMain.java
+++ src/main/java/StudyMain.java
@@ -1,14 +1,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Hello world!
  */
 @SpringBootApplication(scanBasePackages = {"*.*"})
-public class ZaFacilitatorManageApplication {
+public class StudyMain {
     public static void main(String[] args) {
         //System.setProperty("DEPLOY_ENV", "dev");// TODO  上线前请去掉此句，在本机设置好环境变量也可去掉此句
-        SpringApplication.run(ZaFacilitatorManageApplication.class, args);
+        SpringApplication.run(StudyMain.class, args);
     }
 }
